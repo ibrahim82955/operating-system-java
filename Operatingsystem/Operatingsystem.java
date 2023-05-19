28ceea1b97f8bd78470751f52aacc4091e07d9a8
ibrahim82955 <95488741+ibrahim82955@users.noreply.github.com>
2023-05-19 21:15:02 +0300

    Add files via upload

---
+++ Operatingsystem/Operatingsystem.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Operatingsystem;
+
+/**
+ *
+ * @author ibrahim
+ */
+public class Operatingsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     Choose c = new Choose();
+        c.setLocation(200,200);
+        c.setSize(310,350);
+        c.setVisible(true);                                    vxs
+    }
+    
+}
